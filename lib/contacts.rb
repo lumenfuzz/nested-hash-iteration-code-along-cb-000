31e21b98e6c44f64c62f5ae744109fe2471997b6
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-17 00:00:40 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,22 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts = {
+    "Jon Snow" => {
+      name: "Jon",
+      email: "jon_snow@thewall.we",
+      favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+    },
+    "Freddy Mercury" => {
+      name: "Freddy",
+      email: "freddy@mercury.com",
+      favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    }
+  }
+  contacts.each do |tier1_key, tier1_value|
+    tier1_value.each do |tier2_key, tier2_value|
+      if tier2_value == :favorite_ice_cream_flavors
+        data.delete_if {
+          |ice_cream| ice_cream == "strawberry"
+        }
 end
